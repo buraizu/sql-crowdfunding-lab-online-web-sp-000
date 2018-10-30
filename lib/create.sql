4ba9d5a99771f9b9a10118ed8b8d36c8359d15d3
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-30 05:19:52 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,11 +1,11 @@
-CREATE TABLE projects (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  category TEXT,
-  funding_goal REAL,
-  start_date DATE,
-  end_date DATE
-);
+-- CREATE TABLE projects (
+--   id INTEGER PRIMARY KEY,
+--   title TEXT,
+--   category TEXT,
+--   funding_goal REAL,
+--   start_date DATE,
+--   end_date DATE
+-- );
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
